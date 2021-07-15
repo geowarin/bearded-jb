6934951a4ee35d77e3aee33cd99573f73587a29c
Geoffroy Warin <code@geowarin.com>
2021-07-15 21:33:17 +0200

    fix xml colors in template containing hashes and invalid error color. Also generates all Arc themes.

--- templates/main.ts
+++ templates/main.ts
@@ -1,11 +1,44 @@
 import {compile} from "handlebars";
 import {readFileSync, writeFileSync} from "fs";
-import {arc} from "../bearded-theme/src/variations/arc";
+import {Theme} from "../bearded-theme/src/typing";
+import {arc, arcBlueBerry, arcEggplant, arcEolstorm} from "../bearded-theme/src/variations/arc";
 
-function makeTemplate(templateName: string, outFile: string) {
-  const tpl = compile(readFileSync(templateName).toString());
-  writeFileSync(`../src/main/resources/${outFile}`, tpl(arc));
+
+function removeColorHashes<T>(obj: T): T {
+  return Object.fromEntries(
+    Object.entries(obj).map(([key, val]) => {
+        if (typeof val === 'string') {
+          return [key, val.replace("#", "")]
+        } else if (typeof val === 'object') {
+          return [key, removeColorHashes(val)];
+        }
+        return [key, val];
+      }
+    )
+  ) as T
 }
 
-makeTemplate("oneDark.template.theme.json", "bearded-arc.theme.json");
-makeTemplate("one-dark.template.xml", "bearded-arc.xml");
+type TemplateData = Theme & {themeName: string}
+
+function capitalize(word: string) {
+  return word[0].toUpperCase() + word.slice(1);
+}
+
+function makeTemplate(templateName: string, outFile: string, templateData: TemplateData) {
+  const tpl = compile(readFileSync(templateName).toString());
+  const helpers = {
+    "fullName": (name) => `Bearded ${capitalize(name)}`
+  }
+  writeFileSync(`../src/main/resources/${outFile}`, tpl(templateData, {helpers}));
+}
+
+function makeTheme(themeName: string, theme: Theme) {
+  const templateData = {...theme, themeName};
+  makeTemplate("oneDark.template.theme.json", `${themeName}.theme.json`, templateData);
+  makeTemplate("one-dark.template.xml", `${themeName}.xml`, removeColorHashes(templateData));
+}
+
+makeTheme("arc", arc);
+makeTheme("arc-eolstorm", arcEolstorm);
+makeTheme("arc-blueberry", arcBlueBerry);
+makeTheme("arc-eggplant", arcEggplant);
